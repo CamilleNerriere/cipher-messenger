fe9f786f620052816d0f8e67ad465a750797fcee
CamilleNerriere <camille.nerriere@yahoo.fr>
2025-02-12 15:36:00 +0100

    reorganize to monorepo

---
+++ backend/app.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import connectDB from './src/api/data/db.js';
+import { errorHandler, notFound } from './src/api/middlewares/errorHandlers.js';
+import { router } from './src/api/routers/router.js';
+import setupWebSocket from './src/websockets/index.js';
+import limiter from './src/api/middlewares/limiter.js';
+import logger from './logger.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(limiter);
+const server = http.createServer(app);
+
+const wss = setupWebSocket(server);
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: [process.env.REACT_URL],
+    credentials: true,
+    allowedHeaders: ['Authorization', 'Content-Type', 'X-Requested-With'],
+    exposedHeaders: ['Authorization'],
+  })
+);
+
+app.use(router);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  logger.info(`Server is running on port ${process.env.PORT}`);
+});
--- backend/src/api/models/User.js
+++ backend/src/api/models/User.js
@@ -1,5 +1,5 @@
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
@@ -9,8 +9,8 @@ const userSchema = new mongoose.Schema({
 
 // Hash password before saving
 
-userSchema.pre("save", async function (next) {
-  if (this.isModified("password")) {
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
     this.password = await bcrypt.hash(this.password, 10);
   }
   next();
@@ -22,6 +22,6 @@ userSchema.methods.comparePassword = function (password) {
   return bcrypt.compare(password, this.password);
 };
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
 
 export default User;
